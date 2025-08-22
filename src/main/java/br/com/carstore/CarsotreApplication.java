442f35daedf1533731be51db1038109888d30a37
LUCAS GUEDES DE MORAES LACERDA <lucas.gmlacerda@senacsp.edu.br>
2025-08-21 21:27:50 -0300

    Initial commit

---
+++ src/main/java/br/com/carstore/CarsotreApplication.java
@@ -0,0 +1,13 @@
+package br.com.carstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsotreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsotreApplication.class, args);
+	}
+
+}
