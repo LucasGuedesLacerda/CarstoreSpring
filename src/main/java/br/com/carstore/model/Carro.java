3560c6a4c23a20a22fab822c32ca0cb59f12b343
LucasGuedesLacerda <lucasguedeslacerda@gmail.com>
2025-08-28 17:56:56 -0300

    Att

---
+++ src/main/java/br/com/carstore/model/Carro.java
@@ -0,0 +1,19 @@
+package br.com.carstore.model;
+
+public class Carro {
+    private String nome;
+    private String cor;
+
+    public Carro(String nome, String cor) {
+        this.nome = nome;
+        this.cor = cor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+}
